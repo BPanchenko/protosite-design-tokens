2bac746f6540c091b8fb4b5593a746fc3f6d53cf
Boris Vasilievich Panchenko <oberon@list.ru>
2026-03-17 20:47:38 +0300

    update: use `makeCSSColor()` from `chromatic-chandelier/toolkit`

--- source_code/fn.token.ts
+++ source_code/fn.token.ts
@@ -1,10 +1,12 @@
+import { makeCSSColor } from "chromatic-chandelier/toolkit"
 import { getToken } from "./store.ts"
 import { isColorToken } from "./util.token-validator.ts"
 
 export const token = (name: TokenName): Token | string => {
 	let token = getToken(name)
 	if (isColorToken(token)) {
-		return `${token.$value.colorSpace}(${token.$value.components.join(' ')} / ${token.$value.alpha || 1})`
+		let { colorSpace, components, alpha = 1 } = token.$value
+		return makeCSSColor(colorSpace, components, alpha)
 	}
 	return token
 }
