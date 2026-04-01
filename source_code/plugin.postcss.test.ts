a7a59d34080a34c5b45a26067153e74bb774396f
Boris Vasilievich Panchenko <oberon@list.ru>
2026-04-01 23:52:50 +0300

    :bug: the token function don't calls in CSS functions that specified as property value

--- source_code/plugin.postcss.test.ts
+++ source_code/plugin.postcss.test.ts
@@ -27,6 +27,28 @@ describe(
 					}
 				`,
 			],
+			[
+				"grayscale gradient",
+				`
+					@import-design-tokens dark-color-scheme;
+					div {
+						background: repeating-conic-gradient(
+							token(color.white) 0%,
+							token(color.gray.50) 5%,
+							token(color.gray.100) 10%,
+							token(color.gray.200) 20%,
+							token(color.gray.300) 30%,
+							token(color.gray.400) 40%,
+							token(color.gray.500) 50%,
+							token(color.gray.600) 60%,
+							token(color.gray.700) 70%,
+							token(color.gray.800) 80%,
+							token(color.gray.900) 90%,
+							token(color.black) 100%
+						);
+					}
+				`,
+			],
 		]
 
 		for (let [name, source] of testCases)
