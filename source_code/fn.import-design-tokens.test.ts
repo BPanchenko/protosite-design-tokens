f45b3f7472b9c9b7c8aabd315e65a1ced33a1885
Boris Vasilievich Panchenko <oberon@list.ru>
2026-05-18 12:03:17 +0100

    wip<shadow>: testing serialization of some tokens

--- source_code/fn.import-design-tokens.test.ts
+++ source_code/fn.import-design-tokens.test.ts
@@ -2,14 +2,26 @@ import { strictEqual } from 'node:assert';
 import { test } from 'node:test';
 import { importDesignTokens, token } from './index.ts';
 
-test("Import tokens `dark-color-scheme` and check the back and front colors", async (_t: test.TestContext) => {
+test("Import `dark-color-scheme.tokens` and check the back and front colors", async (_t: test.TestContext) => {
 	await importDesignTokens('dark-color-scheme')
 	strictEqual(token('color.background'), token('color.black'))
 	strictEqual(token('color.foreground'), token('color.gray.50'))
 })
 
-test("Import tokens `light-color-scheme` and check the back and front colors", async (_t: test.TestContext) => {
+test("Import `light-color-scheme.tokens` and check the back and front colors", async (_t: test.TestContext) => {
 	await importDesignTokens('light-color-scheme')
 	strictEqual(token('color.background'), token('color.white'))
 	strictEqual(token('color.foreground'), token('color.gray.900'))
+})
+
+test("Check tokens from `shadow.tokens`", async (t: test.TestContext) => {
+	await importDesignTokens('shadow')
+	t.assert.snapshot(token('shadow.1dp'))
+	t.assert.snapshot(token('shadow.2dp'))
+	t.assert.snapshot(token('shadow.3dp'))
+	t.assert.snapshot(token('shadow.4dp'))
+	t.assert.snapshot(token('shadow.6dp'))
+	t.assert.snapshot(token('shadow.8dp'))
+	t.assert.snapshot(token('shadow.16dp'))
+	t.assert.snapshot(token('shadow.24dp'))
 })
